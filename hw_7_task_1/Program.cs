7059bb1804e0d075ad87272bf554c83a7e402403
Kristina <zkvzkvzkv@yandex.ru>
2023-02-05 14:45:49 +0300

    создана программа по условию задачи 1

--- hw_7_task_1/Program.cs
+++ hw_7_task_1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами
+
+double[,] matrix = new double[3, 4];
+
+void PrintMatrix(double[,] matr)              // Печать матрицы
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)                      // Заполнение матрицы рандомными числами
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10 ;             
+        }
+    }
+}
+
+FillArray(matrix);
+PrintMatrix(matrix);
