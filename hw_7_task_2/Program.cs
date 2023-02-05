a04f3e0f4904d28a8826a19c443cd562504a7a00
Kristina <zkvzkvzkv@yandex.ru>
2023-02-05 14:48:25 +0300

    создана программа по условию задачи 2

--- hw_7_task_2/Program.cs
+++ hw_7_task_2/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+Console.WriteLine("Задан массив:");
+PrintMatrix(matrix);
+Console.WriteLine("Введите номер строки");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int column = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[,] matr)                      // Заполнение матрицы рандомными числами
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matr)              // Печать матрицы
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+if (row > matrix.GetLength(0) || column > matrix.GetLength(1))
+{
+    Console.WriteLine("Такого числа в массиве нет");
+}
+else
+{
+    Console.WriteLine($"Искомый элемент равен {matrix[row - 1, column - 1]}");
+}
